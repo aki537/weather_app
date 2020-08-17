109c57e5737968b66566e2d3d0bddad315ef7b88
aki158 <hideponpon1931@gmail.com>
2020-08-17 12:08:08 +0900

    add_user_signup

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ApplicationRecord
+  before_save { self.email.downcase! }
+  validates :name, presence: true, length: {maximum: 50 }
+
+  belongs_to :area
+  
+  validates :email, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
+                    allow_blank: true,
+                    uniqueness: { case_sensitive: false }
+  validates :email, presence: true
+  has_secure_password
+
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_120716) do
+ActiveRecord::Schema.define(version: 2020_08_15_023259) do
 
   create_table "areas", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "prefecture", null: false
@@ -18,4 +18,15 @@ ActiveRecord::Schema.define(version: 2020_08_13_120716) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.bigint "area_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["area_id"], name: "index_users_on_area_id"
+  end
+
+  add_foreign_key "users", "areas"
 end
