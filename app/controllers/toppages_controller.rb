62930845a6e27f0772ed34ac77cd582575826620
aki158 <hideponpon1931@gmail.com>
2020-08-23 22:42:49 +0900

    add_weather_system

--- app/controllers/toppages_controller.rb
+++ app/controllers/toppages_controller.rb
@@ -1,4 +1,26 @@
 class ToppagesController < ApplicationController
   def index
+    if logged_in?
+      @myarea = weather_area(current_user.area.lat.to_f, current_user.area.lon.to_f)
+      @a = y_day
+    end
   end
+  
+  private
+
+  def y_day
+    time = Time.current.tomorrow.to_i
+    lat = current_user.area.lat.to_f
+    lon = current_user.area.lon.to_f
+    yday = weather_area(lat,lon)
+    array = (0..23).to_a
+    
+    array.each do |a|
+      if time < yday["hourly"][a]["dt"]
+        return a
+      end
+    end
+      
+  end
+
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,60 @@
 module ApplicationHelper
+  require 'json'
+  require 'open-uri'
+  require 'date'
+
+  def today
+    d = Date.today
+    month = d.month
+    mday = d.day
+    day = "#{month}月#{mday}日"
+  end
+
+  def fashion_comment(temp)
+    if temp < 6
+      return "ダウンジャケット"
+    elsif temp >= 7 && temp < 11
+      return "厚手のコート"
+    elsif temp >= 12 && temp <15
+      return "薄手のコート、セーター"
+    elsif temp >= 16 && temp < 20
+      return "ジャケット、カーディガン"
+    elsif temp >= 20 && temp < 25
+      return "半袖シャツ、長袖シャツ"
+    elsif temp >=25
+      return "半袖シャツ"
+    end
+    
+  end
+
+  def wind_deg(d)
+    deg = d.to_f
+    if deg > 22.5 && deg <= 67.5
+      return "北東"
+    elsif deg > 67.5 && deg <= 112.5
+      return "東"
+    elsif deg > 112.5 && deg <= 157.5
+      return "南東"
+    elsif deg > 157.5 && deg <= 202.5
+      return "南"
+    elsif deg > 202.5 && deg <= 247.5
+      return "南西"
+    elsif deg > 247.5 && deg <= 292.5
+      return "西"
+    elsif deg > 292.5 && deg <= 337.5
+      return "北西"
+    else deg > 337.5 || deg <= 22.5
+      return "北"
+    end
+  end
+
+  def pop_h(time)
+    pop = @myarea["hourly"][time]["pop"]*100
+  end
+  
+  def hour_c(time)
+    time = Time.at(time).hour
+  end
+  
+  
 end
